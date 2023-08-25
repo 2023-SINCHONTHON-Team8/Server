bfc4d4265931031e030785654bb747e2dbe11e09
jhhyun <jhh980622@naver.com>
2023-08-25 22:32:40 +0900

    model created

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django_enum_choices import EnumChoices
 
-# Create your models here.
+class Taste(EnumChoices):
+    KOREAN = 'Korean'
+    CHINESE = 'Chinese'
+    JAPANESE = 'Japanese'
+    WESTERN = 'Western'
+    World = 'World'
+    Buffet = 'Buffet'
+    Cafe = 'Cafe'
+    Pub = 'Pub'
+
+class Restaurant(models.Model):
+    restaurant_id = models.CharField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    taste = models.CharField(
+        max_length=10,
+        choices=Taste.choices(),
+        default=Taste.KOREAN
+    )
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    menu_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    price = models.CharField(max_length=200)
+    imageurl = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.menu_name
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Restaurant, Menu
 
-# Create your views here.
+def index(request):
+    restaurants = Restaurant.objects.all()
+    context = {'restaurants': restaurants}
+    return render(request, 'restaurant/index.html', context)
+
+def detail(request, restaurant_id):
+    restaurant = Restaurant.objects.get(pk=restaurant_id)
+    menus = restaurant.menu_set.all()
+    context = {'restaurant': restaurant, 'menus': menus}
+    return render(request, 'restaurant/detail.html', context)
