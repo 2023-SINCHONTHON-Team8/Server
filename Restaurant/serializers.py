f148861f8d9ae692ef1d4dec6046ff48d6c34a64
Hanbee Lee <bee1162@naver.com>
2023-08-26 02:54:58 +0900

    fix:status check

--- Restaurant/serializers.py
+++ Restaurant/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Menu
+from .models import Menu, Restaurant
 
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
         fields = '__all__'
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = '__all__'  # 모든 필드를 직렬화
--- User/admin.py
+++ User/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from Post.models import Post
+from Restaurant.models import Restaurant
+from Post.models import GroupTraits
 from User.models import *
 
 # Register your models here.
 admin.site.register(Member)
+admin.site.register(Restaurant)
+admin.site.register(Post)
+admin.site.register(GroupTraits)
+
