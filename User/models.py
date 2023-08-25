5e7b0f09e449238dcc532d81fca990ca84ec04b5
ehyeji <jiyaaaa22@gamil.com>
2023-08-25 23:27:06 +0900

    user

--- User/models.py
+++ User/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class Member(AbstractUser):
+    GENDER_CHOICES = (("male", "남성"), ("female", "여성"))
+    email = models.EmailField(max_length=128, unique=True, verbose_name="사용자이메일")
+    # username = models.CharField(max_length=32, verbose_name="사용자명")
+    password = models.CharField(max_length=64, verbose_name="비밀번호")
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, verbose_name="성별")
+
+    """ manner_temp = 
+    post_list = 
+    spicy = 1~5 """
--- User/views.py
+++ User/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+""" from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import requests
 
 # Create your views here.
+class RegistrationAPIView(APIView):
+    #클라이언트로부터 받은 데이터 처리
+    email = requests.data.get('email')
+    username = requests.data.get('username')
+    password = requests.data.get('password')
+    gender = requests.data.get('gender') #남성/여성 선택
+
+    # Univcert API 호출 및 회원가입 처리
+        # univcert_api_url = ""  
+        univcert_data = {
+            "email": email,
+            "username": username,
+            "password": password,
+            "gender": gender,
+        }
+        
+        response = requests.post(univcert_api_url, json=univcert_data)
+
+        if response.status_code == 200:
+            # 회원가입 성공 시, 여기서 추가적인 처리 (인증 메일 발송 등)
+            return Response({"message": "회원가입이 성공적으로 완료되었습니다."}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "회원가입에 실패하였습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+ """
